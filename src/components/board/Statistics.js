035191bd3b3085ee61c4467b7bccbdcd247e74e0
Adrian Ćwiąkała <adrian.cwiakala@febrit.co>
2022-10-03 10:51:52 +0200

    fix: stack in statistics

--- src/components/board/Statistics.js
+++ src/components/board/Statistics.js
@@ -1,5 +1,14 @@
 import React, { useContext, useState, useEffect, useRef } from "react";
-import { Container, Row, Col, Table, Button, Tabs, Tab } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Table,
+  Button,
+  Tabs,
+  Tab,
+  Stack,
+} from "react-bootstrap";
 import { ACTION } from "../../App";
 import { AppContext } from "../../AppContextProvider";
 import EventEmitter, { E } from "../../utils/events/EventEmitter";
@@ -53,14 +62,56 @@ const Statistics = () => {
                 .map(([k, v]) => {
                   // return <p key={k}>{`k = ${k} v = ${v}`}</p>
                   return (
-                    <div
+                    // <div
+                    //   key={k}
+                    //   className="row mb-1 bg-first pointer hoverable"
+                    //   onClick={() =>
+                    //     EventEmitter.emit(E.onStatClick, { fen: k })
+                    //   }
+                    // >
+                    //   <div className="col-md-9 col-sm-9 col-xs-9 text-sm">
+                    //     {JSON.parse(k).map((g_a, k_g_a) => {
+                    //       const gene = currState.default_genes.find(
+                    //         (g) => g.id === g_a[0]
+                    //       );
+                    //       // return <p key={k_g_a} className="my-0">{gene?.allels[g_a[1][0]].desc}</p>
+                    //       return (
+                    //         <p key={k_g_a} className="my-0">
+                    //           {g_a[1].map((allindx, k_al) => {
+                    //             return (
+                    //               <span
+                    //                 key={k_al}
+                    //               >{`${gene?.allels[allindx].desc}, `}</span>
+                    //             );
+                    //           })}
+                    //         </p>
+                    //       );
+                    //     })}
+                    //   </div>
+
+                    //   <div className="col-md-3 col-md-3 col-xs-3 flex-center text-sm">
+                    //     <p>
+                    //       {currState.cross_data.count_list[k]}/
+                    //       {Object.values(
+                    //         currState.cross_data.count_list
+                    //       ).reduce((acc, v1) => acc + v1, 0)}{" "}
+                    //       (
+                    //       <strong className={getStyle(chance(k))}>
+                    //         {chance(k)}%
+                    //       </strong>
+                    //       )
+                    //     </p>
+                    //   </div>
+                    // </div>
+                    <Stack
+                      direction="horizontal"
                       key={k}
-                      className="row mb-1 bg-first pointer hoverable"
+                      className="p-1 mb-1 bg-first pointer hoverable"
                       onClick={() =>
                         EventEmitter.emit(E.onStatClick, { fen: k })
                       }
                     >
-                      <div className="col-md-9 col-sm-9 col-xs-9 text-sm">
+                      <div>
                         {JSON.parse(k).map((g_a, k_g_a) => {
                           const gene = currState.default_genes.find(
                             (g) => g.id === g_a[0]
@@ -79,8 +130,7 @@ const Statistics = () => {
                           );
                         })}
                       </div>
-
-                      <div className="col-md-3 col-md-3 col-xs-3 flex-center text-sm">
+                      <div className="ms-auto">
                         <p>
                           {currState.cross_data.count_list[k]}/
                           {Object.values(
@@ -93,7 +143,7 @@ const Statistics = () => {
                           )
                         </p>
                       </div>
-                    </div>
+                    </Stack>
                   );
                 })}
             </div>
