b2a09c964802e1ce011d101cd040aadcb9731776
Adrian Ćwiąkała <adrian.cwiakala@febrit.co>
2022-11-16 10:44:36 +0100

    BREAKING CHANGE:  Gamete optimalization and experimental error fix

--- src/utils/CrossFunctions.js
+++ src/utils/CrossFunctions.js
@@ -1,27 +1,36 @@
 const is2dArray = (array) =>
   array.constructor === Array && array.every((item) => Array.isArray(item));
 
-export const getCombinations = (arr) => {
-  if (!is2dArray(arr)) {
+export const getCombinations = (input) => {
+  if (!is2dArray(input)) {
     console.log("Not valid");
     return;
   }
-  let comb = [];
 
-  for (let j = 0; j < Math.pow(arr.length, 2) + 1; j++) {
-    let txt = [];
-    let temp = j;
+  const result = [];
 
-    arr.forEach((v, k) => {
-      txt.push(arr[k][temp & 1]);
-      temp = temp >> 1;
+  const hzCount = input.reduce(
+    (count, current) => count + (current[0] === current[1] ? 0 : 1),
+    0
+  );
+
+  for (let combination = 0; combination < Math.pow(2, hzCount); combination++) {
+    const gametes = [];
+    let currCombination = combination;
+
+    input.forEach((v) => {
+      if (v[0] === v[1]) {
+        gametes.push(v[0]);
+      } else {
+        gametes.push(v[currCombination & 1]);
+        currCombination = currCombination >> 1;
+      }
     });
 
-    if (!comb.find((elem) => JSON.stringify(elem) === JSON.stringify(txt)))
-      comb.push(txt);
+    result.push(gametes);
   }
 
-  return comb;
+  return result;
 };
 
 export const cross = (genA, genB, template_id, state) => {
@@ -34,8 +43,8 @@ export const cross = (genA, genB, template_id, state) => {
       let res = [];
 
       for (let i = 0; i < gametA.length; i++) {
-        if (!gene)
-          gene = state.default_genes.find((g) => g.id === template.gene_ids[i]);
+        // if (!gene)
+        gene = state.default_genes.find((g) => g.id === template.gene_ids[i]);
         const first =
           gene.allels[gametA[i]].prior > gene.allels[gametB[i]].prior
             ? gametA[i]
